039569e1024bf6760d926465e746e0178d830b8e
jerrywzy <jerrywzy@hotmail.com>
2024-03-20 14:54:52 +0800

    Initial movement attempt

--- scripts/pacman.gd
+++ scripts/pacman.gd
@@ -1,23 +1,38 @@
 extends CharacterBody2D
 
-
 var speed = 100.0
-var direction
+var tile_size = 24
+var current_direction = Vector2.ZERO
+var target_direction = Vector2.ZERO
+@onready var raycasts = $Raycasts
+@onready var raycast_a = $Raycasts/RayCast2D
+@onready var raycast_b = $Raycasts/RayCast2D2
+
+func _ready():
+	position = position.snapped(Vector2.ONE * tile_size)
+	position += Vector2.ONE * tile_size/2
+	
 
 func _physics_process(delta):
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	if direction:
-		velocity = direction * speed
-
-	move_and_slide()
+	if target_direction:  # if there is input
+		move(target_direction)  # check target direction if valid
 
 func _input(event):
 	if event.is_action_pressed("left"):
-		direction = Vector2.LEFT
+		raycasts.rotation = PI
+		target_direction = Vector2.LEFT
 	if event.is_action_pressed("right"):
-		direction = Vector2.RIGHT
+		raycasts.rotation = 0
+		target_direction = Vector2.RIGHT
 	if event.is_action_pressed("up"):
-		direction = Vector2.UP
+		raycasts.rotation = -(PI / 2)
+		target_direction = Vector2.UP
 	if event.is_action_pressed("down"):
-		direction = Vector2.DOWN
+		raycasts.rotation = PI / 2
+		target_direction = Vector2.DOWN
+
+func move(target):
+	if not raycast_a.is_colliding() and not raycast_b.is_colliding():
+		position += target * tile_size * 0.1
+	else:
+		pass
